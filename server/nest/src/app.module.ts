ba305823d02fd53589c6ff8be47c436accfd669a
maxdevback <zacharchuk.max.dev@gmail.com>
2023-12-05 11:00:54 +0200

    Added env config and typeorm

--- server/nest/src/app.module.ts
+++ server/nest/src/app.module.ts
@@ -1,15 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
 import { CommentsModule } from './comments/comments.module';
 import { LikesModule } from './likes/likes.module';
 import { NotificationsModule } from './notifications/notifications.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PostsModule, UsersModule, CommentsModule, LikesModule, NotificationsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PostsModule,
+    UsersModule,
+    CommentsModule,
+    LikesModule,
+    NotificationsModule,
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: +process.env.PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: !!process.env.IS_PROD,
+    }),
+  ],
 })
 export class AppModule {}
